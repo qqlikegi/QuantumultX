d6bbea4db767be632ed58093153ea13d11e4309c
qqlikegi <592786274@qq.com>
2023-08-10 19:28:53 +0800

    Update IP.js

--- IP.js
+++ IP.js
@@ -45,5 +45,5 @@ var obj = JSON.parse(body);
 var title = flags.get(obj['countryCode']);
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
-var description = '服务商:' + obj['isp'] + '\n' + '地区:' + City_ValidCheck(obj['regionName']) + '\n' + 'IP:' + obj['query'] + '\n' + '时区:' + obj['timezone'];
+var description = '服务商:' + obj['isp'] + '\n' + '地区:' + obj['regionName'] + '\n' + 'IP:' + obj['query'] + '\n' + '时区:' + obj['timezone'];
 $done({title, subtitle, ip, description});
